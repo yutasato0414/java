789d41ffe567bfedf362474c9123f6afe18ae8d8
taro <taro@example.com>
2022-09-19 21:51:36 +0900

    Java文法1課題 complete

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,9 @@
+public class ArgsLastPrinter{
+  public static void main(String[] args){
+   if(args.length == 0){
+     System.out.println("なし");
+   }else{
+     System.out.println(args[args.length - 1]);
+   }
+  }
+}
---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz{
+        public static void main(String[] args){
+                if(args.length == 0){
+                        return;
+                }
+                int max = Integer.parseInt(args[0]);
+                String result = null;
+                for (int i = 0; i < max; i ++ ){
+                         int num = i + 1;
+                         result = "";
+                         if(num % 3 == 0){
+                                result = result + "Fizz";
+                         }
+                         if(num % 5 == 0){
+                                result = result + "Buzz";
+                         }
+                         if(result.length() == 0){
+                                result = result + num;
+                         }
+                         System.out.print(result + " ");
+                }
+        }
+}
