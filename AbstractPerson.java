1aca32e2acf14dbed5cf2f415c1cec8468306b78
taro <taro@example.com>
2022-09-26 20:35:34 +0900

    20220926 commit

--- AbstractPerson.java
+++ AbstractPerson.java
@@ -1,8 +1,8 @@
 public abstract class AbstractPerson {
 
-    protected abstract String getGender();
+	protected abstract String getGender();
 
-    public void speak(){
-                System.out.println("I'm a " + getGender());
-    }
+	public void speak() {
+		System.out.println("I'm a " + getGender());
+	}
 }
